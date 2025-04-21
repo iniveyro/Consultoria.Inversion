448b6267fb2ef057a7d5394357df869ae85a10f7
iniveyro <iiniveyro@gmail.com>
2025-04-21 11:23:24 -0300

    Modificacion de certificaciones en base a los Brokers.

--- src/core/Consultoria.Inversion.Domain/Enums/Certificaciones.cs
+++ src/core/Consultoria.Inversion.Domain/Enums/Certificaciones.cs
@@ -2,9 +2,14 @@ namespace Consultoria.Inversion.Domain.Enums
 {
     public enum Certificaciones
     {
-        CPA,
-        CFP,
-        IAEF,
-        Ninguna,
+        CNVA,	//Certificación de Idoneidad – CNV (Argentina)	Requerida por la CNV para actuar como agente
+        CFA,	//Chartered Financial Analyst	Certificación internacional muy prestigiosa
+        CFP,	//Certified Financial Planner	Enfocada en planificación financiera personal
+        Series7,	//FINRA Series 7 (USA)	Licencia para operar valores, requerida en EE.UU.
+        Series63,	//FINRA Series 63 (USA)	Permite operar a nivel estatal
+        AMB,	//Agente de Mercado de Bonos (BYMA, Argentina)	Especializado en renta fija
+        ALyC,	//Agente de Liquidación y Compensación	Permite operar en mercados regulados argentinos
+        MIFID,	//MiFID II Compliance (UE)	Norma europea para brindar asesoramiento financiero
+        NA	//Capacitación interna de la entidad
     }
 }
