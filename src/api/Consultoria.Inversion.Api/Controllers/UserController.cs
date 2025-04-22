055943cdba76433057e59e5b9cf2c90fe6bf5198
iniveyro <iiniveyro@gmail.com>
2025-04-22 10:36:52 -0300

    Se implemento control de errores de tipo 500.

--- src/api/Consultoria.Inversion.Api/Controllers/UserController.cs
+++ src/api/Consultoria.Inversion.Api/Controllers/UserController.cs
@@ -1,10 +1,12 @@
-using Consultoria.Inversion.Api.Features;
+using Consultoria.Inversion.Application.Features;
 using Microsoft.AspNetCore.Mvc;
+using Consultoria.Inversion.Application.Exceptions;
 
 namespace Consultoria.Inversion.Api.Controllers
 {
     [ApiController]
     [Route("api/v1/user")]
+    [TypeFilter(typeof(ExcepctionManager))]
     public class UserController : ControllerBase
     {
         public UserController()
@@ -13,8 +15,9 @@ namespace Consultoria.Inversion.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> test ()
+        public async Task<IActionResult> Test()
         {
+            var number = int.Parse("asdasd");
             return StatusCode(StatusCodes.Status200OK, ResponseApiService.Response(StatusCodes.Status200OK,"{}","Se ejecuto correctamente"));
         }        
     }
